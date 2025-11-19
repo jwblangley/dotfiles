318b1c3b54eb08b48f10f199fa817a35e1a03528
James Langley <jwblangley@gmail.com>
2025-11-19 21:19:59 +0000

    Write vim shortcut to swap between .cpp and .h files

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -9,3 +9,17 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+
+-- .cpp <-> .h shortcut
+vim.keymap.set("n", "<leader>ch", function()
+    local current = vim.api.nvim_buf_get_name(0)
+    local target = current:gsub("%.cpp$", ".h")
+    if target == current then
+        target = current:gsub("%.h$", ".cpp")
+    end
+    if target == current then
+        vim.notify("File is not .cpp or .h", vim.logl.levels.WARN)
+        return
+    end
+    vim.cmd("edit " .. target)
+end, { desc = "Toggle between .cpp and .h" })
