9cc5637f28f71a83098d2006e5dead4f8367f5d2
James Langley <jwblangley@gmail.com>
2026-06-13 17:38:46 +0100

    Add telescope resume shortcut

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -12,6 +12,7 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "Telescope resume" })
 
 -- .cpp <-> .h shortcut
 vim.keymap.set("n", "<leader>ch", function()
